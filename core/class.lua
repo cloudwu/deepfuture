69da5f008024c60978288517e235162fac07597e
Cloud Wu <cloudwu@gmail.com>
2025-08-26 17:22:05 +0800

    small fix

--- core/class.lua
+++ core/class.lua
@@ -2,18 +2,23 @@ local class = {}; setmetatable(class, class)
 
 function class:__index(name)
 	local class_methods = {}; class_methods.__index = class_methods
-	local class_object = {
+	local class_object = {}
+	local class_meta = {
 		__newindex = class_methods,
 		__index = class_methods,
 		__call = function(self, init)
 			return setmetatable(init or {}, class_methods)
 		end
 	}
-	class[name] = setmetatable(class_object, class_object)
+	class[name] = setmetatable(class_object, class_meta)
 	return class_object
 end
 
 local class = { __index = class }; setmetatable(class, class)
+
+function class:__call(name)
+	return self[name]
+end
 
 local function container_next(self, k)
 	local nk, v = next(self, k)
